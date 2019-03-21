131d7ad554982a597b45c3b846404b4bba15a9b6
Alistair Blake <consultations@me.com>
2019-03-20 19:31:25 -0700

    initial update made

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const PORT = 8080;
 
 app.use(express.static('./public'));
 
-app.listen(PORT, () => {
+app.listen(process.env.PORT, () => {
   console.log('Web Server up on port', PORT);
 });
