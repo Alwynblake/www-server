7a71e2e9af94ea7d2dae203ccf22ac56cb9ffe9d
Alistair Blake <consultations@me.com>
2019-03-20 21:42:06 -0700

    getting redirect error

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 'use strict';
 
-require('dotenv').config();
 const express = require('express');
 const app = express();
-const PORT = 8080;
+require('dotenv').config();
+// const PORT = 8080;
 
 app.use(express.static('./public'));
 
 app.listen(process.env.PORT, () => {
-  console.log('Web Server up on port', PORT);
+  console.log('Web Server up on port', process.env.PORT);
 });
